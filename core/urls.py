d41f748f87a49b2fabf49b87f711a50c877d4e63
Leanstix <leanstixx@gmail.com>
2025-04-11 18:27:45 +0100

    Deployed. And Sam is gay

--- core/urls.py
+++ core/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('', lambda request: JsonResponse({'message': 'Welcome to the WMS API!'})),
+    path('', lambda request: JsonResponse({'message': 'Sam is gay'})),
 ]
 
 if settings.DEBUG: 
