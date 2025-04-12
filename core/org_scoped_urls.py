1d7d8cea31f68b41174310e72739fd4a0c4f8eb3
Leanstix <leanstixx@gmail.com>
2025-04-12 08:50:45 +0100

    making organization scoped urls

--- core/org_scoped_urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path("login/", include("users.org_urls")),         # users/views/login tied to org
-    path("bookings/", include("booking.org_urls")),    # bookings tied to org
-]
