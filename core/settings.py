e77f2104866cad5c8eb64aa6606ae23a822c71df
Leanstix <leanstixx@gmail.com>
2025-04-12 22:50:12 +0100

    setting

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+PORT = config("PORT", 8000)
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
