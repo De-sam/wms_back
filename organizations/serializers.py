b908ffebb7642abaa279a7aedf7205df8d940d5b
Leanstix <leanstixx@gmail.com>
2025-04-13 08:37:29 +0100

    testing all

--- organizations/serializers.py
+++ organizations/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Organization, User
-from django.utils.text import slugify
 
 
 class OrganizationSignupSerializer(serializers.ModelSerializer):
@@ -9,14 +8,4 @@ class OrganizationSignupSerializer(serializers.ModelSerializer):
         fields = ['organization_name', 'email']
 
     def create(self, validated_data):
-        # Generate a unique subdomain
-        base_subdomain = slugify(validated_data['organization_name'])
-        # Ensure the subdomain is unique    
-        subdomain = base_subdomain
-        count = 1
-        while Organization.objects.filter(subdomain=subdomain).exists():
-            subdomain = f"{base_subdomain}{count}"
-            count += 1
-        validated_data['subdomain'] = subdomain
-
         return Organization.objects.create(**validated_data)
