0aa07d8df98f945d8444a50cecf3dee47d18f6e4
Leanstix <leanstixx@gmail.com>
2025-04-21 10:24:48 +0100

    fixed

--- workspace/apps.py
+++ workspace/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
 
-def ready(self):
-    import .signals
-
-
 class WorkspaceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'workspace'
