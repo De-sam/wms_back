b25cbd22509eee2de1fe30e72531f756c03c873f
Leanstix <leanstixx@gmail.com>
2025-04-21 10:26:27 +0100

    work

--- workspace/apps.py
+++ workspace/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 class WorkspaceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'workspace'
+
+    def ready(self):
+        import workspace.signals  # Correct relative import
+        # Ensure signals are imported when the app is ready
+        # This is important to ensure that the signals are registered
+        # and will work as expected when the app is running.
