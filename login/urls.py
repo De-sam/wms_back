f671c3580deaaf3772869c2a38b678785c935820
Leanstix <leanstixx@gmail.com>
2025-04-22 10:55:01 +0100

    combined the login

--- login/urls.py
+++ login/urls.py
@@ -4,7 +4,7 @@ from .views import LoginView, AdminLoginView, CombinedLoginView
 app_name = "login"
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='organization-login'),
+    #path('login/', LoginView.as_view(), name='organization-login'),
     #path('admin/login/', AdminLoginView.as_view(), name='admin-login'),
     path('admin/login/', CombinedLoginView.as_view(), name='admin-login'),
 ]
